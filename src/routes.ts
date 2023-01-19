19ad1bb532c12c13a177cc6f287f1e0f7cebe949
kesantos <kesantos@paschoalotto.com.br>
2023-01-18 21:54:13 -0300

    feat(Rota): Criado rota patch

--- src/routes.ts
+++ src/routes.ts
@@ -70,4 +70,51 @@ export async function appRoutes(app: FastifyInstance) {
       completeHabits,
     }
   })
+
+  app.patch('/habits/:id/toogle', async (request) => {
+    const toogleHabitsParams = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = toogleHabitsParams.parse(request.params)
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      })
+    }
+  })
 }
